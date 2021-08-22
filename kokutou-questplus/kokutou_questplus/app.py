3da593ce596545fc2889ba07b0b142514fecf4b7
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2021-08-22 16:50:21 +0900

    fix

--- kokutou-questplus/kokutou_questplus/app.py
+++ kokutou-questplus/kokutou_questplus/app.py
@@ -1,41 +1,37 @@
 import json
 import urllib.parse
+from typing import List
+
+import questplus as qp
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    qp_params = json.loads(urllib.parse.unquote(event["queryStringParameters"]["qp_params"]))["qp_params"]
+    results = json.loads(urllib.parse.unquote(event["queryStringParameters"]["results"]))["results"]
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+    return run(qp_params, sorted(results, key=lambda x: x["num_trial"]))
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
 
-    context: object, required
-        Lambda Context runtime methods and attributes
+def run(qp_params: dict, results: List[dict]) -> dict:
+    intensity: List[float] = qp_params["stim_domain"]["intensity"]
+    if len(intensity) < 2:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"message": "too few intensities"}),
+        }
 
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    qp_params = urllib.parse.unquote(event['queryStringParameters']['qp_params'])
-    results = urllib.parse.unquote(event['queryStringParameters']['results'])
-    print(qp_params)
-    print(results)
+    q = qp.QuestPlus(**qp_params)
+    for res in results:
+        q.update(
+            stim=dict(intensity=int(res["move_width"]) / 10),
+            outcome=dict(response=res["response"])
+        )
 
     return {
         "statusCode": 200,
         "body": json.dumps(
             {
-                "message": "hello world",
-                "qp_params": qp_params,
-                "results": results,
+                "next_stim": q.next_stim["intensity"],
             }
         ),
     }
