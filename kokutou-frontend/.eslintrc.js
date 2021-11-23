068a1b2fd8442d740d09451314714e25591a9064
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2021-11-23 19:12:21 +0900

    Adopt to ESLint setting
    Use sinmple eslint setting.
    Tweek the following rules to refrain from excessive pointing out.
    - react/jsx-filename-extension
    - react/react-in-jsx-scope
    - func-names
    - react/jsx-props-no-spreading

--- kokutou-frontend/.eslintrc.js
+++ kokutou-frontend/.eslintrc.js
@@ -1,24 +1,33 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    'env': {
+        'browser': true,
+        'es2021': true,
     },
-    "extends": [
-        "plugin:react/recommended",
-        "airbnb"
+    'extends': [
+        'plugin:react/recommended',
+        'airbnb',
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true,
         },
-        "ecmaVersion": 12,
-        "sourceType": "module"
+        'ecmaVersion': 12,
+        'sourceType': 'module',
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
+    'plugins': [
+        'react',
+        '@typescript-eslint',
     ],
-    "rules": {
-    }
+    'rules': {
+        'react/jsx-filename-extension': [
+            'error',
+            {
+                extensions: ['.jsx', '.tsx'],
+            },
+        ],
+        'react/react-in-jsx-scope': 'off',
+        'func-names': 'off',
+        'react/jsx-props-no-spreading': 'off',
+    },
 };
