8b3bec86ca73ce7e3d5600fd274253baf9431aae
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2021-11-25 20:23:31 +0900

    Add indent setting to fix the coding style

--- kokutou-frontend/.eslintrc.js
+++ kokutou-frontend/.eslintrc.js
@@ -1,39 +1,41 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true,
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
     },
-    'extends': [
-        'plugin:react/recommended',
-        'airbnb',
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint',
+  ],
+  'rules': {
+    'indent': ['error', 2],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
     ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true,
-        },
-        'ecmaVersion': 12,
-        'sourceType': 'module',
-    },
-    'plugins': [
-        'react',
-        '@typescript-eslint',
-    ],
-    'rules': {
-        'react/jsx-filename-extension': [
-            'error',
-            {
-                extensions: ['.jsx', '.tsx'],
-            },
-        ],
-        'react/react-in-jsx-scope': 'off',
-        'func-names': 'off',
-        'react/jsx-props-no-spreading': 'off',
-        'react/function-component-definition': ['error', {
-            'namedComponents': 'function-declaration',
-            'unnamedComponents': 'arrow-function',
-        }],
-        'no-use-before-define': 'off',
-        '@typescript-eslint/no-use-before-define': ['error'],
-    },
+    'react/react-in-jsx-scope': 'off',
+    'func-names': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/function-component-definition': ['error', {
+      'namedComponents': 'function-declaration',
+      'unnamedComponents': 'arrow-function',
+    }],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'import/extensions': 'off',
+  },
 };
